653f7167d7eb3b345dccb8b5aa3b37182472f595
Renata Mitsue <rmkarato@gmail.com>
2020-09-11 00:46:03 -0300

    new tables

--- src/routes/fieldsRouter.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const FieldsController = require("../controllers/FieldsController");
-
-const fieldsRouter = express.Router();
-
-fieldsRouter.post("/:farm_id/register", FieldsController.registerField);
-fieldsRouter.get("/all", FieldsController.getAllFields);
-
-module.exports = fieldsRouter;
