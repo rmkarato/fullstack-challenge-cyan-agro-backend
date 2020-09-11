fbe64ed90ec55c4dacb054fba1c6718a9839e150
Renata Mitsue <rmkarato@gmail.com>
2020-09-11 02:03:37 -0300

    .

--- src/config/database.ts
+++ src/config/database.ts
@@ -6,8 +6,16 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_DATABASE_NAME,
+    port: process.env.PORT,
     define: {
         timestamps: true,
         underscored: true,
+    },
+    pool: {
+        max: 15,
+        min: 5,
+        idle: 20000,
+        evict: 15000,
+        acquire: 30000
     },
 };
